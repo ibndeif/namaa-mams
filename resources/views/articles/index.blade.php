2273072043fca91afb663a0abd7d7215caec6e9d
Ahmed Galal <ibndeif@gmail.com>
2023-09-18 01:26:57 +0300

    add create article button and adjust table design

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,7 +2,9 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Articles') }}
+            <a href="{{route('articles.create')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 float-right">Create Article</a>
         </h2>
+
     </x-slot>
 
     <x-section>
@@ -35,9 +37,9 @@
                     <th scope="col" class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase">Action</th>
                 </tr>
             </thead>
-            <tbody class="divide-y divide-gray-200 dark:divide-gray-500">
+            <tbody class="divide-y divide-gray-200 dark:divide-gray-200">
                 @foreach($articles as $article)
-                <tr class="hover:bg-gray-100 dark:hover:bg-gray-500">
+                <tr class="hover:bg-gray-100 dark:hover:bg-gray-200">
                     <td class="px-6 py-4 whitespace-nowrap text-sm"><img src="{{ $article->image }}" class="object-fill" alt=""></td>
                     <td class=" px-6 py-4 whitespace-nowrap text-sm">{{ Str::limit($article->title, 50) }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm">{{ $article->author->name }}</td>
